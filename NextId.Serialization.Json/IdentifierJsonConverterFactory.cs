cfff92a477afdd40a3c8e144f44654a0afd62417
stanac <stanacevm@gmail.com>
2023-07-02 12:18:38 +0200

    serialization support in progress

--- NextId.Serialization.Json/IdentifierJsonConverterFactory.cs
+++ NextId.Serialization.Json/IdentifierJsonConverterFactory.cs
@@ -19,17 +19,3 @@ public class IdentifierJsonConverterFactory : JsonConverterFactory
         return (JsonConverter)Activator.CreateInstance(typeof(IdentifierJsonConverter<>).MakeGenericType(typeToConvert))!;
     }
 }
-
-public class IdentifierJsonConverter<T> : JsonConverter<T>
-    where T: Identifier<T>
-{
-    public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        throw new NotImplementedException();
-    }
-
-    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
-    {
-        throw new NotImplementedException();
-    }
-}
